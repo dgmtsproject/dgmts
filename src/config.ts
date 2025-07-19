72ac201a07cc71128d96e69dd02995c246938a7b
dgmtsproject <dgmts.project@gmail.com>
2025-07-19 16:38:54 +0500

    backend changed Url endpoint for SSL fix

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
-export const API_BASE_URL = "https://71.178.209.68:5000"; 
+export const API_BASE_URL = "https://imsite.dullesgeotechnical.com"; 
 
-// export const API_BASE_URL ='http://localhost:5000';
+// export const API_BASE_URL ='http://localhost:5000';`
